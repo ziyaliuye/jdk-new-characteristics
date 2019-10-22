d5278b8b14836a7e5c54b3ea3b99f1e46d9af924
ziyaliuye <171242039@qq.com>
2019-10-22 20:19:36 +0800

    first commit...

---
+++ jdk10-upgrade/src/cn/jdk10/upgrade/Demo.java
@@ -0,0 +1,33 @@
+package cn.jdk10.upgrade;
+
+/*
+     ┏┛ ┻━━━━━┛ ┻┓
+     ┃　　　　　　 ┃
+     ┃　　　━　　　┃
+     ┃　┳┛　  ┗┳　┃
+     ┃　　　　　　 ┃
+     ┃　　　┻　　　┃
+     ┃　　　　　　 ┃
+     ┗━┓　　　┏━━━┛
+       ┃　　　┃   神兽保佑
+       ┃　　　┃   永无BUG..
+       ┃　　　┗━━━━━━━━━┓
+       ┃　　　　　　　    ┣┓
+       ┃　　　　         ┏┛
+       ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+         ┃ ┫ ┫   ┃ ┫ ┫
+         ┗━┻━┛   ┗━┻━┛
+*/
+
+/**
+ * @author lele
+ * @version 1.0
+ * @Description
+ * @Email 414955507@qq.com
+ * @date 2019/10/22 20:19
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
