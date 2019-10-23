77be0bbd95f9bb636052c3d7849a88dd899fa98a
ziyaliuye <171242039@qq.com>
2019-10-23 21:56:47 +0800

    first commit...

--- jdk12-13-upgrade/src/cn/jdk12/upgrade/Demo.java
+++ jdk12-13-upgrade/src/cn/jdk12/upgrade/Demo.java
@@ -25,6 +25,13 @@ package cn.jdk12.upgrade;
         BUG闪开...
 */
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  * @author lele
  * @version 1.0
@@ -33,7 +40,7 @@ package cn.jdk12.upgrade;
  * @date 2019/10/23 14:13
  */
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
          * JDK12中增强了switch-case语法（在原有的匹配符 : 基础上增加了Lambda表达式符 ->）：
          *  使用 -> 的可以省去了break语句，避免了因少写break而出错， 同时兼容原有的 : 表达式（不能省掉break）
@@ -49,5 +56,34 @@ public class Demo {
             default -> "月份有误....";
         };
         System.out.println(quarter);
+
+        /* 支持压缩数字格式化  */
+        // 例如，在en_US语言环境中，1000可以格式化为“1K”，1000000可以格式化为“1M”
+        // 具体取决于指定的样式NumberFormat.Style
+        var cnf = NumberFormat.getCompactNumberInstance(Locale.CHINA, NumberFormat.Style.SHORT);
+        System.out.println(cnf.format(10000));
+        System.out.println(cnf.format(25000));
+        System.out.println(cnf.format(1000000));
+        System.out.println(cnf.format(1000000000));
+
+        /* String新增transform()，indent()方法  */
+        // transform(Function)此方法可以将单个String做类似于map的映射操作
+        // indent(int) 将str缩进指定个空格， str有多行时每一行都缩进
+        String str = "hello".transform(r -> r.toUpperCase()).transform(r -> r.indent(3));
+        System.out.println(str);
+
+        /* Files新增mismatch()方法, 可以返回两个Path对应的文件内容从第一个开始不一样的地方的索引， 如果都一样返回-1 */
+        // Files是操作Path的工具类， Path是NIO2推出的替代File类的， Paths类用于实例化Path类
+        FileWriter fileWriter = new FileWriter("tmp\\a.txt");
+        fileWriter.write("a");
+        fileWriter.write("b");
+        fileWriter.write("c");
+        fileWriter.close();
+        FileWriter fileWriterB = new FileWriter("tmp\\b.txt");
+        fileWriterB.write("a");
+        fileWriterB.write("1");
+        fileWriterB.write("c");
+        fileWriterB.close();
+        System.out.println(Files.mismatch(Path.of("tmp/a.txt"), Path.of("tmp/b.txt")));
     }
 }
